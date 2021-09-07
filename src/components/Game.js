2e7d9a158159a0818f5ae0582d09b28236b90566
Dhairya Bhatia <dhairyab@geekyants.com>
2021-09-07 23:28:09 +0530

    feat: Added winner status and VS computer mode switch UI

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,21 +7,45 @@ import { calculateWinner } from "../helpers";
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXisNext] = useState(true);
+  const [openSpots, setOpenSpots] = useState(9);
 
-  const winner = calculateWinner(board);
+  const printWinner = () => {
+    if (!winner) {
+      if (openSpots > 0) {
+        return null;
+      } else {
+        return `It's a Tie`;
+      }
+    } else {
+      return `${winner} Wins!`;
+    }
+  };
+  let winner = calculateWinner(board);
 
   const handleClick = (i) => {
     const boardCopy = [...board];
     if (winner || boardCopy[i]) return;
 
     boardCopy[i] = xIsNext ? "X" : "O";
+    setOpenSpots(openSpots - 1);
     setBoard(boardCopy);
     setXisNext(!xIsNext);
   };
   return (
     <div>
       <h1 className="heading"> Tic Tac Toe !</h1>
-      <Board boxes={board} onClick={handleClick} />
+      <div className="parent-container">
+        <div class="container">
+          <h2>VS Computer Mode</h2>
+          <label class="switch">
+            <input type="checkbox" /> <div></div>
+          </label>
+        </div>
+        <div>
+          <Board boxes={board} onClick={handleClick} />
+          <h2 className="winner">{printWinner()}</h2>
+        </div>
+      </div>
     </div>
   );
 };
